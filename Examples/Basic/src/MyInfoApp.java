d173429939ecb729d7c85b5ddbe47c8ddbc0dee2
jyajoo <jyajoo1020@gmail.com>
2022-06-02 21:30:48 +0900

    add: Example MyInfoApp

---
+++ Examples/Basic/src/MyInfoApp.java
@@ -0,0 +1,12 @@
+public class MyInfoApp {
+    public static void main(String[] args) {
+        String name = "YunAh Jung";
+        Integer age = 23;
+        String major = "Department of Information and Communication Engineering";
+
+
+        System.out.println(name);
+        System.out.println(age + " years old");
+        System.out.println(major);
+    }
+}
