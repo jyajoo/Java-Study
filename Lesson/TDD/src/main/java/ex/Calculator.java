f908e381f3c86c629cff66890976b1f8423d7d61
jyajoo <jyajoo1020@gmail.com>
2022-07-15 10:07:53 +0900

    add: minus & multi & div

--- Lesson/TDD/src/main/java/ex/Calculator.java
+++ Lesson/TDD/src/main/java/ex/Calculator.java
@@ -2,11 +2,60 @@ package ex;
 
 public class Calculator {
 
-    public static int run(String s) {
+    public static int calc(String s) {
+        boolean isMinus = s.indexOf(" - ") != -1;
+        boolean isMulti = s.indexOf(" * ") != -1;
+        boolean isDivi = s.indexOf(" / ") != -1;
+
+
+        if (isMinus) {
+            return minus(s);
+        } else if (isMulti) {
+            return multi(s);
+        } else if (isDivi) {
+            return divi(s);
+        }
+        return add(s);
+
+    }
+
+
+    private static int add(String s) {
         String[] split = s.split(" \\+ ");
         int no1 = Integer.parseInt(split[0]);
         int no2 = Integer.parseInt(split[1]);
-
         return no1 + no2;
     }
+
+    private static int minus(String s) {
+        String[] split = s.split(" \\- ");
+        int no1 = Integer.parseInt(split[0]);
+        int no2 = Integer.parseInt(split[1]);
+        return no1 - no2;
+    }
+
+    private static int multi(String s) {
+        String[] split = s.split(" \\* ");
+        int no1 = Integer.parseInt(split[0]);
+        int no2 = Integer.parseInt(split[1]);
+        return no1 * no2;
+    }
+
+    private static int divi(String s) {
+        String[] split = s.split(" \\/ ");
+        int no1 = Integer.parseInt(split[0]);
+        int no2 = Integer.parseInt(split[1]);
+        return no1 / no2;
+    }
+
+
+//            "1 + 1"
+//            "1 + 2"
+//            "1 - 2"
+//            "1 * 2"
+//            "4 / 2"
+//            "1 + 2 + 3"
+//            "1 + 2 * 3"
+//            "(1 + 2) * 3"
+
 }
--- Lesson/TDD/src/test/java/ex/CalculatorTest.java
+++ Lesson/TDD/src/test/java/ex/CalculatorTest.java
@@ -7,19 +7,50 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
     @Test
     public void 더하기(){
-        int run = Calculator.run("10 + 20");
-        assertEquals(30, run);
+        int calc = Calculator.calc("10 + 20");
+        assertEquals(30, calc);
     }
 
     @Test
     public void 더하기_2() {
-        int run = Calculator.run("20 + 20");
-        assertEquals(40, run);
+        int calc = Calculator.calc("20 + 20");
+        assertEquals(40, calc);
     }
 
     @Test
     public void 더하기_3() {
-        int run = Calculator.run("10 + 10");
-        assertEquals(20, run);
+        int calc = Calculator.calc("10 + 10");
+        assertEquals(20, calc);
+    }
+
+    @Test
+    public void 빼기(){
+        int calc = Calculator.calc("10 - 10");
+        assertEquals(0, calc);
+    }
+
+    @Test
+    public void 곱하기() {
+        int calc = Calculator.calc("10 * 10");
+        assertEquals(100, calc);
+    }
+
+    @Test
+    public void 나누기() {
+        int calc = Calculator.calc("100 / 10");
+        assertEquals(10, calc);
+    }
+
+    @Test
+    public void 음수_테스트() {
+        int calc = Calculator.calc("-10 + -10");
+        int calc2 = Calculator.calc("-10 - 10");
+        int calc3 = Calculator.calc("-10 * 10");
+        int calc4 = Calculator.calc("-10 / 10");
+
+        assertEquals(-20, calc);
+        assertEquals(-20, calc2);
+        assertEquals(-100, calc3);
+        assertEquals(-1, calc4);
     }
 }
